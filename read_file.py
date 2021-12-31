f7296f86b2777680d4219cf77828b7a26680630f
chengkahohk <46778468+chengkahohk@users.noreply.github.com>
2021-12-31 20:08:41 +0800

    master

--- read_file.py
+++ read_file.py
@@ -1,3 +1,8 @@
 f = open('my_path/my_file.txt', 'r')
 file_data = f.read()
+print(file_data)
 f.close()
+
+with open('my_path/my_file.txt', 'r') as f:
+    file_data = f.read()
+    print(file_data)
